d43d760872a6d6885de5ffa52c6d39418d67baff
sdivers <sdivers@users.noreply.github.com>
2017-03-09 15:48:09 -0500

    Update boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -26,11 +26,11 @@ erb $APP_ROOT/nginx/conf/orig.conf > $APP_ROOT/nginx/conf/nginx.conf
 
 # Do the variable replacement for any nginx config file
 services_folder=$APP_ROOT/nginx/services/conf
-echo "List of files in conf: `ls -l $services_folder`"
 if [ -d "$services_folder" ]; then
   #shopt -s nullglob
   for file in $services_folder/*.conf; do
     if [ -f $file ]; then
+      echo "File: $file"
       mv $file $services_folder/temp.conf
       erb $services_folder/temp.conf $file
     fi
